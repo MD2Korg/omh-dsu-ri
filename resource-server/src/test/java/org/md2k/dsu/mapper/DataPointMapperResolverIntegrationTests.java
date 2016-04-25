81bcee3cbecf08df766edb925a5c89fd2d9215f7
Emerson Farrugia <emerson@openmhealth.org>
2016-04-25 16:49:34 -0700

    Fix data point mapper resolver integration tests
    The tests didn't reflect the refactored inheritance
    hierarchy.

--- resource-server/src/test/java/org/md2k/dsu/mapper/DataPointMapperResolverIntegrationTests.java
+++ resource-server/src/test/java/org/md2k/dsu/mapper/DataPointMapperResolverIntegrationTests.java
@@ -17,42 +17,33 @@
 package org.md2k.dsu.mapper;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.md2k.dsu.configuration.DataPointMapperSettings;
-import org.openmhealth.dsu.configuration.TransientIntegrationTestConfiguration;
 import org.openmhealth.schema.domain.omh.DataPoint;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.Collections;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.Collections.nCopies;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 
 /**
+ * @param <I> the input type of the mappers
  * @author Emerson Farrugia
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {
-        TransientIntegrationTestConfiguration.class,
-        DataPointMapperResolverIntegrationTests.class
-})
-@Configuration
-@ComponentScan(basePackageClasses = DataPointMapper.class)
-public class DataPointMapperResolverIntegrationTests {
+public abstract class DataPointMapperResolverIntegrationTests<I> {
 
-    @Autowired
-    private DataPointMapperResolver mapperResolver;
+    /**
+     * @return the resolver to test
+     */
+    protected abstract DataPointMapperResolver<I> getResolver();
 
 
     @Test(expected = IllegalArgumentException.class)
-    public void constructorShouldFailOnIdentifierConflicts() {
+    public void constructorShouldFailOnIdentifierConflicts()
+            throws Throwable {
 
         DataPointMapper mapper = new DataPointMapper() {
             @Override
@@ -66,19 +57,24 @@ public class DataPointMapperResolverIntegrationTests {
             }
         };
 
-        new DataPointMapperResolver(Collections.nCopies(2, mapper));
+        try {
+            getResolver().getClass().getConstructor(List.class).newInstance(nCopies(2, mapper));
+        }
+        catch (InvocationTargetException e) {
+            throw e.getCause(); // unwrap the invocation target exception to get to the actual cause
+        }
     }
 
     @Test
     public void getMapperShouldReturnEmptyOnUnknownMapperId() {
 
-        assertThat(mapperResolver.getMapper("nonExistent").isPresent(), equalTo(false));
+        assertThat(getResolver().getMapper("nonExistent").isPresent(), equalTo(false));
     }
 
     @Test
     public void getMapperShouldReturnMapperOnValidMapperId() {
 
-        Optional<DataPointMapper> mapper = mapperResolver.getMapper(DefaultDataSampleDataPointMapper.MAPPER_ID);
+        Optional<DataPointMapper<I>> mapper = getResolver().getMapper(DefaultDataSampleDataPointMapper.MAPPER_ID);
 
         assertThat(mapper.isPresent(), equalTo(true));
         assertThat(mapper.get().getIdentifier(), equalTo(DefaultDataSampleDataPointMapper.MAPPER_ID));
